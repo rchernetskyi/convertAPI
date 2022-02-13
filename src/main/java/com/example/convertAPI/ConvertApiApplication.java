dd7f3e9c75b70edf8f1356b6951c4bc8904c4654
Roman Chernetskiy <romachernetskiy@gmail.com>
2022-02-13 14:46:52 +0200

    Initial commit

---
+++ src/main/java/com/example/convertAPI/ConvertApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.convertAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/convertAPI/ConvertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
